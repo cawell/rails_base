b984a3405e2d0f5dc772b3c7cfecfce7a0925bfd
Wellington Caetano <wl.a.caetano@gmail.com.br>
2022-02-20 15:30:14 -0300

    Fix: simple test for request root

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Homes", type: :request do
+RSpec.describe 'Homes', type: :request do
   let(:url) { 'http://localhost/' }
 
-  describe "GET /" do
-    it "return status 200" do
+  describe 'GET /' do
+    it 'return status 200' do
       get url
 
       expect(response).to have_http_status(200)
