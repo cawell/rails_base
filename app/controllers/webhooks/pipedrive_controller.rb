3542d94c2551057b8ffe5d801e10755359ec331a
Wellington Caetano <wl.a.caetano@gmail.com>
2024-11-12 17:13:54 -0300

    feat: add webhook

---
+++ app/controllers/webhooks/pipedrive_controller.rb
@@ -0,0 +1,17 @@
+module Webhooks
+  class PipedriveController < ApplicationController
+    before_action :authenticate_pipedrive
+
+    def receive
+      Rails.logger.info("Received parameters: #{params.to_unsafe_h}")
+    end
+
+    private
+
+    def authenticate_pipedrive
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV["WEBHOOK_USERNAME"] && password == ENV["WEBHOOK_PASSWORD"]
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#show'
+
+  namespace :webhooks do
+    post "/pipedrive", to: "pipedrive#receive"
+  end
 end
